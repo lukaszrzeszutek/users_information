a5a75446ab10dfa3b3b0b9e73f2f058d204b8b8f
lukaszrzeszutek <31247526+lukaszrzeszutek@users.noreply.github.com>
2017-12-09 01:02:01 +0100

    Add files via upload

---
+++ search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Przeszukiwanie plików konfiguracyjnych Linuxa
+
+echo "Podaj nazwę użytkownika"
+read uzytkownik
+
+echo "Podaj ścieżkę do pliku"
+read sciezka
+
+spr=$(cat $sciezka | grep $uzytkownik || echo "Nie ma użytkownika $uzytkownik w pliku $sciezka")
+
+echo $spr
+
+
